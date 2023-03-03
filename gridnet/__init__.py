eace09c5f23733e26351dbbf5c471eda1d0ea89f
Rohit Singh <rohitsingh@gmail.com>
2023-03-03 18:49:14 -0500

    blah

--- gridnet/__init__.py
+++ gridnet/__init__.py
@@ -2,14 +2,16 @@ __version__ = "0.1.0"
 __citation__ = """Singh, Wu, Berger. "Granger causal inference on DAGs identifies genomic loci regulating transcription."  ICLR 2022. 
  Wu, Singh, Walsh, Berger. "An econometric lens resolves cell-state parallax." bioRxiv."""
 from . import (
-    gridnet,
+    train,
     utils,
 )
 
-from utils import construct_dag, infer_knngraph_pseudotime, dag_orient_edges, load_multiome_data
-from gridnet import run_gridnet
+from .utils import construct_dag, infer_knngraph_pseudotime, dag_orient_edges, load_multiome_data
+from .train import run_gridnet
 
 __all__ = [
+    "train",
+    "utils",
     "construct_dag",
     "infer_knngraph_pseudotime",
     "dag_orient_edges",
